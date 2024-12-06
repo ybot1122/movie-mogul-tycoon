7335e2529e94ee9a8629099e5b64c3d2228274b8
Toby Liu <ybot1122@gmail.com>
2024-12-05 18:38:58 -0800

    Propose movie with timer

--- project/movie_mogul_tycoon/lib/model/game_brain.dart
+++ project/movie_mogul_tycoon/lib/model/game_brain.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:movie_mogul_tycoon/model/movie_proposal.dart';
 
 const oneSecond = Duration(seconds: 1);
@@ -21,32 +22,55 @@ final movieProposals = <MovieProposal>[
   MovieProposal(
     budget: 250000,
     title: "Mario World Wonder",
+    boxOfficeReturn: 300000,
     releaseDate: DateTime(1990, DateTime.january, 1),
+    proposalDate: DateTime(1989, DateTime.december, 30),
   ),
   MovieProposal(
     budget: 350000,
+    boxOfficeReturn: 20000,
     title: "Big Time Boom Boom",
     releaseDate: DateTime(1990, DateTime.february, 1),
+    proposalDate: DateTime(1990, DateTime.january, 2),
   ),
   MovieProposal(
     budget: 500000,
+    boxOfficeReturn: 1000000,
     title: "This Is",
     releaseDate: DateTime(1990, DateTime.march, 1),
+    proposalDate: DateTime(1990, DateTime.january, 30),
   ),
 ];
 
 class GameBrain {
   final startDate = DateTime.utc(1989, DateTime.december, 25);
+  var currentDate = DateTime.utc(1989, DateTime.december, 25);
 
   Timer? gameClock;
   int currentTick = 0;
   int currentMoney = 5000000;
+  bool gameClockPaused = false;
 
   // Call once at start of game
   start(updateCb) {
     gameClock = Timer.periodic(oneSecond, (timer) {
+      if (gameClockPaused) return;
+
       currentTick += 1;
+      currentDate = currentDate.add(const Duration(days: 1));
+
       updateCb();
+
+      // Check if Movie is being proposed on this day
+      Iterable<MovieProposal> mps = movieProposals
+          .where((m) => DateUtils.isSameDay(m.proposalDate, currentDate));
+
+      if (mps.isNotEmpty) {
+        // make movie proposal
+        MovieProposal mp = mps.first;
+        String title = mp.title;
+        print('Proposing movie: $title');
+      }
     });
   }
 
@@ -56,7 +80,6 @@ class GameBrain {
   }
 
   getCurrentDay() {
-    var currentDate = startDate.add(Duration(days: currentTick));
     var day = currentDate.day;
     var month = months[currentDate.month - 1];
     var year = currentDate.year;
--- project/movie_mogul_tycoon/lib/model/movie_proposal.dart
+++ project/movie_mogul_tycoon/lib/model/movie_proposal.dart
@@ -1,8 +1,14 @@
 class MovieProposal {
   int budget;
+  int boxOfficeReturn;
   String title;
   DateTime releaseDate;
+  DateTime proposalDate;
 
   MovieProposal(
-      {required this.budget, required this.title, required this.releaseDate});
+      {required this.budget,
+      required this.title,
+      required this.boxOfficeReturn,
+      required this.releaseDate,
+      required this.proposalDate});
 }
